6ba54bd58b4c4a7c7e8f23c8d652ec0fcb3d23bc
Vitaliy Abovyan <abovyan_vitaly@mail.ru>
2024-12-19 21:27:34 +0300

    new commit

--- src/main/java/ru/qa/scooters/OrderPage.java
+++ src/main/java/ru/qa/scooters/OrderPage.java
@@ -1,4 +1,11 @@
 package ru.qa.scooters;
 
+import org.openqa.selenium.By;
+
 public class OrderPage {
+    public static final By NAME_INPUT = By.xpath(".//input[@placeholder=\"* Имя\"]");
+    public static final By FAMILY_INPUT = By.xpath(".//input[@placeholder=\"* Фамилия\"]");
+    public static final By ADDRESS_INPUT = By.xpath(".//input[@placeholder=\"* Адрес: куда привезти заказ\"]");
+    public static final By METRO_INPUT = By.className("select-search__input");
+    public static final By PHONE_NUMBER_INPUT = By.xpath(".//input[@placeholder=\"* Телефон: на него позвонит курьер\"]");
 }
